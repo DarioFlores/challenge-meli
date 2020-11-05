8cfe1b60975e8966d1d1205030853688dc898802
DarioFlores <darioexequiel22@gmail.com>
2020-11-04 21:39:38 -0300

    Agregando servicio REST y completando funcionalidad de descifrar el mensaje

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# challenge-meli
+# Challenge MeLi - Dario Exequiel Flores
+
+## Como ejecutar el programa?
+* 1 - Debe tener instalado Go (https://golang.org/)
+* 2 - Instalar el paquete 'github.com/gorilla/mux'
+
+    ``go get -u github.com/gorilla/mux``
+
+* 3 - Ahora puede ejecutar el programa de la carpeta raiz del proyecto con el comando:
+
+    ``go run main.go``
--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
 	"math"
-	"strconv"
+	"net/http"
 )
+
+const DESPLAZAMIENTO_ANGULAR = 0.05
+const ERROR_PRECISION = 5
+
+type errorResponse struct {
+	Message string `json:"Message"`
+}
 
 type coordenada struct {
 	X float32 `json:"X"`
@@ -15,10 +27,29 @@ type satelite struct {
 	Nombre string `json:"Nombre"`
 	Posicion coordenada `json:"Posicion"`
 }
-//Posición de los satélites actualmente en servicio
-//● Kenobi: [-500, -200]
-//● Skywalker: [100, -100]
-//● Sato: [500, 100]
+
+type sateliteRequest struct {
+	Name string `json:"Name"`
+	Distance float32 `json:"Distance"`
+	Message []string `json:"Message"`
+}
+
+type infoNave struct {
+	Satellites []sateliteRequest `json:"Satellites"`
+}
+
+
+type createInfoNave struct {
+	Distance float32 `json:"Distance"`
+	Message []string `json:"Message"`
+}
+
+var infoSatelites []sateliteRequest
+
+type responseInfoNave struct {
+	Position coordenada `json:"Position"`
+	Message string `json:"Message"`
+}
 
 var satelitesFuncionando = []satelite{
 	{
@@ -44,6 +75,7 @@ var satelitesFuncionando = []satelite{
 	},
 }
 
+
 func gradosARadianes(deg float64) float64 {
 	return deg * (math.Pi / 180.0)
 }
@@ -63,19 +95,16 @@ func calcularCircunferencia(distancia float32, centroCirculo coordenada) []coord
 	var circunferencia []coordenada
 	for angulo < dosPi {
 		circunferencia = append(circunferencia, calcularCoordenadas(distancia, angulo, centroCirculo))
-		angulo += gradosARadianes(0.1) // se desplaza 0.05°
+		angulo += gradosARadianes(DESPLAZAMIENTO_ANGULAR) // se desplaza 0.05°
 	}
 	return circunferencia
 }
 
 func comparar(coo1 coordenada, coo2 coordenada) bool {
-	//x2 := math.Pow(float64(coo2.X - coo1.X), 2)
-	//y2 := math.Pow(float64(coo2.Y - coo1.Y), 2)
-	//d := math.Sqrt(x2 + y2)
 	x := math.Abs(float64(coo1.X - coo2.X))
 	y := math.Abs(float64(coo1.Y - coo2.Y))
-	valorX := x < 5
-	valorY := y < 5
+	valorX := x < ERROR_PRECISION
+	valorY := y < ERROR_PRECISION
 	if valorX && valorY {
 		return true
 	}
@@ -110,51 +139,185 @@ func promedioCoordenadas(puntos []coordenada) coordenada {
 	}
 }
 
-func puntoComunCircunferencias(circunferencias ...[]coordenada) (coordenada, string) {
+func puntoComunCircunferencias(circunferencias ...[]coordenada) (coord coordenada, err error) {
 	var puntosComunes = circunferencias[0]
 	for i := 1; i < len(circunferencias); i++ {
 		puntosComunes = compararCoordenadas(puntosComunes, circunferencias[i])
 	}
-	for _, comune := range puntosComunes {
-		fmt.Println(comune)
-	}
 	if len(puntosComunes) > 0 {
 		coordenadasNave := promedioCoordenadas(puntosComunes)
-		return coordenadasNave, ""
+		return coordenadasNave, nil
 	}
-	return coordenada{X: 0,	Y: 0}, "Algo salio Mal"
+	return coordenada{X: 0,	Y: 0}, errors.New("algo salio mal")
 }
 
-func pasarFloat32AString(valor float32) string {
-	return fmt.Sprintf("%.1f", valor)
+func validarNombreSatelites(name string) bool {
+	for _, s := range satelitesFuncionando {
+		if s.Nombre == name {
+			return true
+		}
+	}
+	return false
 }
 
-func pasarStringAFloat32(valor string) (float32, error) {
-	float, err := strconv.ParseFloat(valor, 32)
+
+func infoNaveHandler(writer http.ResponseWriter, request *http.Request) {
+	var infoNave infoNave
+	var resInfo responseInfoNave
+	var errorsRes []errorResponse
+	reqBody, err := ioutil.ReadAll(request.Body)
+	writer.Header().Set("Content-Type", "application/json")
+
 	if err != nil {
-		return 0, err
+		_, _ = fmt.Fprintf(writer, "Ocurrio un problema")
 	}
-	return float32(float), err
+	err = json.Unmarshal(reqBody, &infoNave)
+	if err != nil {
+		_, _ = fmt.Fprintf(writer, "Ocurrio un problema")
+	}
+	if len(infoNave.Satellites) == 3 {
+		for _, satellite := range infoNave.Satellites {
+			if !validarNombreSatelites(satellite.Name) {
+				e := errorResponse{Message: "No se encontro satelite en funcionamiento con nombre:" + satellite.Name}
+				errorsRes = append(errorsRes, e)
+			}
+		}
+		if len(errorsRes) != 0 {
+			writer.WriteHeader(http.StatusBadRequest)
+			_ = json.NewEncoder(writer).Encode(errorsRes)
+		} else {
+			var distancias []float32
+			var mensajes [][]string
+			ordenSatelites := []string{"Kenobi", "Skywalker", "Sato"}
+			for _, nameSate := range ordenSatelites {
+				for _, satellite := range infoNave.Satellites {
+					if nameSate == satellite.Name {
+						distancias = append(distancias, satellite.Distance)
+						mensajes = append(mensajes, satellite.Message)
+					}
+				}
+			}
+			msg, erroMensajes := GetMessage(mensajes...)
+			x, y, errorLocation := GetLocation(distancias...)
+			if erroMensajes != nil || errorLocation != nil {
+				writer.WriteHeader(http.StatusNotFound)
+				//_ = json.NewEncoder(writer).Encode(error.Error())
+			} else {
+				resInfo = responseInfoNave{
+					Position: coordenada{
+						X: x,
+						Y: y,
+					},
+					Message:  msg,
+				}
+				writer.WriteHeader(http.StatusCreated)
+				_ = json.NewEncoder(writer).Encode(resInfo)
+			}
+		}
+	} else {
+		writer.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(writer).Encode(errorResponse{Message: "Pasar una cantidad de 3 satelites exactamente"})
+	}
+}
+
+func createInfoNaveHandler(writer http.ResponseWriter, request *http.Request) {
+	var newInfoNave createInfoNave
+	var errorsResponse []errorResponse
+	reqBody, err := ioutil.ReadAll(request.Body)
+	vars := mux.Vars(request)
+	name, ok := vars["satellite_name"]
+	if !ok {
+		_, _ = fmt.Fprintf(writer, "Ocurrio un problema")
+	}
+	writer.Header().Set("Content-Type", "application/json")
+
+	if err != nil {
+		_, _ = fmt.Fprintf(writer, "Ocurrio un problema")
+	}
+	err = json.Unmarshal(reqBody, &newInfoNave)
+	if err != nil {
+		_, _ = fmt.Fprintf(writer, "Ocurrio un problema")
+	}
+
+	if !validarNombreSatelites(name) {
+		e := errorResponse{Message: "No se encontro satelite en funcionamiento con nombre:" + name}
+		errorsResponse = append(errorsResponse, e)
+	}
+	for _, sate := range infoSatelites {
+		if sate.Name == name {
+			e := errorResponse{Message: "Satelite " + name + " ya existe"}
+			errorsResponse = append(errorsResponse, e)
+		}
+	}
+	if len(infoSatelites) == 3 {
+		e := errorResponse{Message: "Solo se pueden crear 3 satelites"}
+		errorsResponse = append(errorsResponse, e)
+	}
+	if len(errorsResponse) != 0 {
+		writer.WriteHeader(http.StatusBadRequest)
+		_ = json.NewEncoder(writer).Encode(errorsResponse)
+	} else {
+		info := sateliteRequest{
+			Name:     name,
+			Distance: newInfoNave.Distance,
+			Message:  newInfoNave.Message,
+		}
+		infoSatelites = append(infoSatelites, info)
+
+		writer.WriteHeader(http.StatusCreated)
+
+	}
+}
+
+func readInfoNaveHandler(writer http.ResponseWriter, request *http.Request) {
+	var resInfo responseInfoNave
+	writer.Header().Set("Content-Type", "application/json")
+
+	if len(infoSatelites) == 3 {
+		var distancias []float32
+		var mensajes [][]string
+		ordenSatelites := []string{"Kenobi", "Skywalker", "Sato"}
+		for _, nameSate := range ordenSatelites {
+			for _, satellite := range infoSatelites {
+				if nameSate == satellite.Name {
+					distancias = append(distancias, satellite.Distance)
+					mensajes = append(mensajes, satellite.Message)
+				}
+			}
+		}
+		msg, erroMensajes := GetMessage(mensajes...)
+		x, y, errorLocation := GetLocation(distancias...)
+		if erroMensajes != nil || errorLocation != nil {
+			writer.WriteHeader(http.StatusNotFound)
+			//_ = json.NewEncoder(writer).Encode(error.Error())
+		} else {
+			resInfo = responseInfoNave{
+				Position: coordenada{
+					X: x,
+					Y: y,
+				},
+				Message:  msg,
+			}
+			writer.WriteHeader(http.StatusCreated)
+			_ = json.NewEncoder(writer).Encode(resInfo)
+		}
+	} else {
+		writer.WriteHeader(http.StatusNotFound)
+	}
 }
 
 func main() {
-	x, y := GetLocation(670.82, 583.1, 761.58)
-	fmt.Println(x, y)
+	//x, y := GetLocation(670.82, 583.1, 761.58)
+	//fmt.Println(x, y)
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.HandleFunc("/topsecret", infoNaveHandler).Methods("POST")
+	router.HandleFunc("/topsecret_split/{satellite_name}", createInfoNaveHandler).Methods("POST")
+	router.HandleFunc("/topsecret_split", readInfoNaveHandler).Methods("GET")
+
+	log.Fatal(http.ListenAndServe(":8090", router))
 }
-
-func foo()  {
-	var x float32 = 36.1135843521
-	fmt.Println("String:", x)
-	fmt.Printf("%T\n", x)
-	s := pasarFloat32AString(x)
-	fmt.Println("String:", s)
-	fmt.Printf("%T\n", s)
-	f, _ := pasarStringAFloat32(s)
-	fmt.Println("Float32", f)
-	fmt.Printf("%T\n", f)
-}
-
-
 
 //Posición de los satélites actualmente en servicio
 //● Kenobi: [-500, -200]
@@ -163,19 +326,62 @@ func foo()  {
 
 // input: distancia al emisor tal cual se recibe en cada satélite
 // output: las coordenadas ‘x’ e ‘y’ del emisor del mensaje
-func GetLocation(distances ...float32) (x, y float32){
+func GetLocation(distances ...float32) (x, y float32, err error){
 	// Se considera que viene la distancia de los satelites en el orden Kenobi, Skywalker y Sato
 	// y siempre vienen 3 distancias
 	kenobiCirculo := calcularCircunferencia(distances[0], satelitesFuncionando[0].Posicion)
 	skywalkerCirculo := calcularCircunferencia(distances[1], satelitesFuncionando[1].Posicion)
 	satoCirculo := calcularCircunferencia(distances[2], satelitesFuncionando[2].Posicion)
 	coordenadaNave, err := puntoComunCircunferencias(kenobiCirculo, skywalkerCirculo, satoCirculo)
-	if err != "" {
-		fmt.Println(err)
+	if err != nil {
+		return 0, 0, err
 	}
-	return coordenadaNave.X, coordenadaNave.Y
+	return coordenadaNave.X, coordenadaNave.Y, nil
+}
+
+
+func descifrar(palabras ...string) string {
+	indecifrable := false
+	var palabraRes = ""
+	for _, p := range palabras {
+		if p != "" {
+			// p no viene vacia
+			if palabraRes != ""{
+				// palabraRes ya tiene algo
+				if palabraRes != p{
+					// si las palabras no coinciden
+					indecifrable = true
+					palabraRes = ""
+				}
+			} else {
+				// palabraRes no tiene nada
+				if !indecifrable {
+					// Aun no hubo inconsistencia
+					palabraRes = p
+				}
+			}
+		}
+	}
+	return palabraRes
 }
 
 // input: el mensaje tal cual es recibido en cada satélite
 // output: el mensaje tal cual lo genera el emisor del mensaje
-//func GetMessage(messages ...[]string) (msg string) {}
+func GetMessage(messages ...[]string) (msg string, err error) {
+	mismoTam := len(messages[0]) == len(messages[1]) && len(messages[1]) == len(messages[2])
+	tam := len(messages[2])
+	if mismoTam {
+		for i := 0; i < tam; i++ {
+			if (i + 1) == tam {
+				msg += descifrar(messages[0][i], messages[1][i], messages[2][i])
+			} else {
+				msg += descifrar(messages[0][i], messages[1][i], messages[2][i]) + " "
+			}
+		}
+		return msg, nil
+	} else {
+		return "", errors.New("no se puede descifrar el mensaje")
+	}
+}
+
+
